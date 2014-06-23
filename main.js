6c6081c66fb0b0f7de5cfc5ea357f129af1241d2
Teri <tealynsea@hotmail.com>
2014-06-23 17:26:40 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+ 
+ var phoneNumber = prompt("Please enter your phone number.  Your number will be used for educational purposes only.  Please follow the xxx-xxx-xxxx format.");
+
+ if (phoneNumber.length != 12) {
+ 		alert("Please follow the xxx-xxx-xxxx format.");
+
+ }
+
+   else  {
+   		alert("Please proceed.");
+   }
+
+var birthDate = prompt("Please enter your birth date.  Follow the xx/xx/xx format.");
+
+if (birthDate.length != 8) {
+		alert("Please follow the xx/xx/xx format.");
+}
+
+else {
+	alert("Please proceed.");
+}
+
+var postalCode = prompt("Please enter your zip code.  You may follow the xxxxx or xxxxx-xxxx.");
+
+
